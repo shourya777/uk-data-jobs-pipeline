35ef6daa4d3cc39c37f8954f6264e00fc68f1115
shourya777 <shourya777@users.noreply.github.com>
2026-08-20 07:43:36 +0000

    deploy: ea1c4c0da2f70058655717c43313cc1a69b26099

--- run/uk_data_jobs_pipeline/snapshots/job_postings_snapshot.sql/job_postings_snapshot.sql
+++ run/uk_data_jobs_pipeline/snapshots/job_postings_snapshot.sql/job_postings_snapshot.sql
@@ -1,46 +1,17 @@
 
+      update "warehouse"."snapshots"."job_postings_snapshot" as DBT_INTERNAL_TARGET
+    set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
+    from "job_postings_snapshot__dbt_tmp20260820074330707780" as DBT_INTERNAL_SOURCE
+    where DBT_INTERNAL_SOURCE.dbt_scd_id::text = DBT_INTERNAL_TARGET.dbt_scd_id::text
+      and DBT_INTERNAL_SOURCE.dbt_change_type::text in ('update'::text, 'delete'::text)
       
-    
-
-    create  table
-      "warehouse"."snapshots"."job_postings_snapshot"
-  
-    
-    as (
+        and DBT_INTERNAL_TARGET.dbt_valid_to is null;
       
-    
 
-    select *,
-        md5(coalesce(cast(posting_id as varchar ), '')
-         || '|' || coalesce(cast(now()::timestamp as varchar ), '')
-        ) as dbt_scd_id,
-        now()::timestamp as dbt_updated_at,
-        now()::timestamp as dbt_valid_from,
-        
-  
-  coalesce(nullif(now()::timestamp, now()::timestamp), null)
-  as dbt_valid_to
-from (
-        
+    insert into "warehouse"."snapshots"."job_postings_snapshot" ("posting_id", "job_title", "company_name", "location_display", "salary_min", "salary_max", "apply_url", "posted_at", "dbt_updated_at", "dbt_valid_from", "dbt_valid_to", "dbt_scd_id")
+    select DBT_INTERNAL_SOURCE."posting_id",DBT_INTERNAL_SOURCE."job_title",DBT_INTERNAL_SOURCE."company_name",DBT_INTERNAL_SOURCE."location_display",DBT_INTERNAL_SOURCE."salary_min",DBT_INTERNAL_SOURCE."salary_max",DBT_INTERNAL_SOURCE."apply_url",DBT_INTERNAL_SOURCE."posted_at",DBT_INTERNAL_SOURCE."dbt_updated_at",DBT_INTERNAL_SOURCE."dbt_valid_from",DBT_INTERNAL_SOURCE."dbt_valid_to",DBT_INTERNAL_SOURCE."dbt_scd_id"
+    from "job_postings_snapshot__dbt_tmp20260820074330707780" as DBT_INTERNAL_SOURCE
+    where DBT_INTERNAL_SOURCE.dbt_change_type::text = 'insert'::text;
 
 
-
-select
-    posting_id,
-    job_title,
-    company_name,
-    location_display,
-    salary_min,
-    salary_max,
-    apply_url,
-    posted_at
-from "warehouse"."staging"."stg_job_postings"
-
-    ) sbq
-
-
-
-    );
-    
-  
   
